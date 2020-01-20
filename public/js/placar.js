1f618b6cdc0da5079d2a052e7255adfe738be6c7
Joao Moreira <jonnes04@hotmail.com>
2020-01-19 23:30:16 -0300

    Fim do projeto, adicionada página index, e recursos de bibliotecas complementares do jquery.

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,8 +1,9 @@
 $("#botao-placar").click(mostraPlacar);
+$("#botao-sync").click(sincronizaPlacar);
 
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Douglas"
+    var usuario = $("#usuarios").val();
     var numPalavras = $("#contador-palavras").text();
 
     var linha = novaLinha(usuario, numPalavras);
@@ -53,4 +54,48 @@ function removeLinha() {
 
 function mostraPlacar() {
     $(".placar").stop().slideToggle(600);
+}
+
+
+function sincronizaPlacar(){
+    var placar = [];
+    var linhas = $("tbody>tr");
+    
+
+    linhas.each(function(){
+        var usuario = $(this).find("td:nth-child(1)").text();
+        var palavras = $(this).find("td:nth-child(2)").text();
+
+        var score = {
+            usuario: usuario,
+            pontos: palavras
+        }
+        placar.push(score);
+    });
+    
+
+    $.post("http://localhost:3000/placar", {placar: placar}, function(){
+        $(".tooltip").tooltipster("open");
+    })
+    .fail(function(){
+        $(".tooltip").tooltipster("open").tooltipster("content", "Falha ao sincronizar");
+    })
+    .always(function(){
+        setTimeout(function(){
+            $(".tooltip").tooltipster("close");
+        }, 1200)
+    })
+}
+
+function atualizaPlacar(){
+    $.get("http://localhost:3000/placar", function(data){
+
+        $(data).each(function(){
+            var linha = novaLinha(this.usuario, this.pontos);
+            linha.find(".botao-remover").click(removeLinha);
+            $("tbody").append(linha);
+        })
+    });
+
+
 }
